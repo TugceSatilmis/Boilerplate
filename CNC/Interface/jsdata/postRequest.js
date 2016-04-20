ed6af66a5469e3d26dc8e67bbff1863096bbc6ed
Stomson <1425157@stud.hs-mannheim.de>
2016-04-20 15:59:50 +0200

    postrequest #1
    Noch nicht implementiert, noch nicht getestet, noch nicht kommentiert.

---
+++ CNC/Interface/jsdata/postRequest.js
@@ -0,0 +1,18 @@
+/*
+Onlick mit ; zuwischen befehlen terennen.
+*/
+function senden(button_id){ 
+	var sen = null;
+	if(sen == null){
+		sen = new XMLHttpRequest();
+	}
+	var taste = document.getElementById(button_id);
+	if(taste.innerHTML == 'Start'){
+		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
+		sen.send('ID, stop');
+	}
+	else{
+		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
+		sen.send('ID, start');
+	}
+};
