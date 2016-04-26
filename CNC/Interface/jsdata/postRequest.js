8a3ab4b1843d4bacda6995340b035a6310012586
Stomson <1425157@stud.hs-mannheim.de>
2016-04-26 10:03:42 +0200

    kleiner schritt
    Das der workflow net ganz am abschmieren ist XD

--- CNC/Interface/jsdata/postRequest.js
+++ CNC/Interface/jsdata/postRequest.js
@@ -1,7 +1,19 @@
 /*
-
+zu erst wird eine Json erstellt
+dann wird per je nach button status per XHR eine post request auf den server gestertet
+als inhalt wird die zu beginn genutzte JSon übergeben
 */
 function senden(button_id){ 
+	var zahl;
+	var art;
+	var inhalt;
+	var json = {
+	id: zahl,
+	type: art,
+	data: {
+		inhalt
+	}
+};
 	var sen = null;
 	if(sen == null){
 		sen = new XMLHttpRequest();
@@ -9,10 +21,10 @@ function senden(button_id){
 	var taste = document.getElementById(button_id);
 	if(taste.innerHTML == 'Start'){
 		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
-		sen.send('ID, stop');
+		sen.send(json);
 	}
 	else{
 		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
-		sen.send('ID, start');
+		sen.send(json);
 	}
 };
