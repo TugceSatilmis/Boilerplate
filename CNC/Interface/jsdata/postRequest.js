3a7e223d4bfa8d687992cca1884c2f7276c76b8d
Stomson <1425157@stud.hs-mannheim.de>
2016-05-01 21:07:51 +0200

    wieder was gemacht

--- CNC/Interface/jsdata/postRequest.js
+++ CNC/Interface/jsdata/postRequest.js
@@ -3,26 +3,20 @@ zu erst wird eine Json erstellt
 dann wird per je nach button status per XHR eine post request auf den server gestertet
 als inhalt wird die zu beginn genutzte JSon übergeben
 */
-function senden(button_id){ 
-	var json = {
-	id: zahl,
-	type: art,
-	data: {
-		inhalt
-		}
-	};
-	var sen = null;
-	if(sen == null){
-		sen = new XMLHttpRequest();
-		sen.responseType = 'json';
-	}
-	var taste = document.getElementById(button_id);
-	if(taste.innerHTML == 'Start'){
-		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
-		sen.send(json);
-	}
-	else{
-		sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status', true);
-		sen.send(json);
-	}
+//8d5e8be2efb2756510f8daf76b1094b2   token
+
+var sendata;
+  
+var senden = function(id, comand) {
+  var sen = new XMLHttpRequest();
+  sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status');
+  sen.responseType = 'json';
+  sen.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+  sen.setRequestHeader('X-CSRF-TOKEN', '8d5e8be2efb2756510f8daf76b1094b2');
+  sen.onload = function() {
+	sendata = { "id": id, "action": comand };
+  };
+  
+  sen.send(sendata);
+  return true;
 };
