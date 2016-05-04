c0fddf1399cc42fa35536c04bcb7044cd5ff0461
Stomson <1425157@stud.hs-mannheim.de>
2016-05-04 15:24:32 +0200

    Update postRequest.js

--- CNC/Interface/jsdata/postRequest.js
+++ CNC/Interface/jsdata/postRequest.js
@@ -7,16 +7,16 @@ als inhalt wird die zu beginn genutzte JSon übergeben
 
 var sendata;
   
-var senden = function(id, comand) {
+var senden = function(type, comand) {
   var sen = new XMLHttpRequest();
-  sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Status');
+  sen.open('POST', 'http://botnet.artificial.engineering:8080/api/Tasks');
   sen.responseType = 'json';
   sen.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-  sen.setRequestHeader('X-CSRF-TOKEN', '8d5e8be2efb2756510f8daf76b1094b2');
+  sen.setRequestHeader('TOKEN', '8d5e8be2efb2756510f8daf76b1094b2');
   sen.onload = function() {
-	sendata = { "id": id, "action": comand };
+	//für später
   };
-  
+  sendata = {"action": comand , "type": type};
   sen.send(sendata);
   return true;
 };
