96d2f98f8e668eeb1479d2b8a632bfed390c9d19
Stomson <1425157@stud.hs-mannheim.de>
2016-06-12 18:54:24 +0200

    test

--- CNC/Interface/jsdata/aktualisieren.js
+++
@@ -1,8 +0,0 @@
-/*
-Diese funktion aktualisiert jede sekunden halbe sekunde (möglichst life) die tabellen
-*/
-
-var aktualisieren = function(){
-	setInterval(function () {tabelleninhalt();}, 5000);
-	setInterval(function () {taskTabelleninhalt();}, 5000);
-};
